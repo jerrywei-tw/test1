c58d291951b3e98eb9fef5d64a7d76eb189fdb9f
jerry <web.jerry1@gmail.com>
2025-08-17 18:54:17 +0800

    test1

--- test1/main.py
+++ test1/main.py
@@ -1,6 +1,13 @@
-def main():
-    print("Hello from test1!")
 
+
+
+
+import pprint
+
+def main():
+
+    print("Hello from test1!")
+    pprint.pprint("Hello from test1!")
 
 if __name__ == "__main__":
     main()
